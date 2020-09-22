c8f8654659e5fdc73acb90c800093edd04ace256
Mert Gül <ibrahimmertgul@gmail.com>
2020-09-22 10:52:04 +0300

    gestures removed from here

--- lib/Color_item.dart
+++ lib/Color_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-// ignore: must_be_immutable
+typedef OnSelected = void Function(int);
+
 class ColorItem extends StatefulWidget {
   bool isSelected;
   final int index;
@@ -8,48 +9,32 @@ class ColorItem extends StatefulWidget {
   final double boxWidth;
   final double boxRadius;
   final double horizontalPadding;
+  final OnSelected onSelected;
 
   ColorItem({
+    Key key,
     this.backgroundColor,
+    @required this.onSelected,
     this.isSelected = false,
     this.boxWidth = 50,
     this.boxRadius = 5,
     this.horizontalPadding = 3,
     @required this.index,
-  });
-  myExport() => createState().export();
+  }) : super(key: key);
+
   @override
   _ColorItemState createState() => _ColorItemState();
 }
 
 class _ColorItemState extends State<ColorItem> {
-  bool export() {
-    //TODO BLOC ÖĞRENİLECEK
-    return widget.isSelected;
+  void resetState() {
+    setState(() {
+      widget.isSelected = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: widget.horizontalPadding),
-      child: GestureDetector(
-        onTap: () {
-          setState(() {
-            widget.isSelected = !widget.isSelected;
-          });
-        },
-        child: Container(
-          width: widget.boxWidth,
-          decoration: BoxDecoration(
-            color: widget.backgroundColor,
-            borderRadius: BorderRadius.circular(widget.boxRadius),
-          ),
-          child: Icon(
-            Icons.check,
-            color: widget.isSelected ? Colors.white : widget.backgroundColor,
-          ),
-        ),
-      ),
-    );
+    return Container();
   }
 }
